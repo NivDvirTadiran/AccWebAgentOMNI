e216beca6d66d122029538138c890004f8fe0071
NivDvirTadiran <nivdvirtadirantele@gmail.com>
2024-08-14 13:51:04 +0300

    task# 0000 - ACC OMNI | Chromatic adjustment.

--- accapi_client/src/polyfills.ts
+++ accapi_client/src/polyfills.ts
@@ -53,10 +53,10 @@ import 'core-js/es/reflect';
  * Zone JS is required by default for Angular itself.
  */
 // import 'zone.js/dist/zone'; // Included with Angular CLI.
-import { NativeEventSource, EventSourcePolyfill } from 'event-source-polyfill';
-(window as any).global = window;
+//import { NativeEventSource, EventSourcePolyfill } from 'event-source-polyfill';
+//(window as any).global = window;
 //(window as any).global.Buffer = (window as any).global.Buffer || require("buffer").Buffer;
-(window as any).global.EventSource =  NativeEventSource || EventSourcePolyfill;
+//(window as any).global.EventSource =  NativeEventSource || EventSourcePolyfill;
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
